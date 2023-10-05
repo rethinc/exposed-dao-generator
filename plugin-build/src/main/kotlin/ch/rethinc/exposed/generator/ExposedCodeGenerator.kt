0b5427fedcc1757fd26a8cf6157df452ea521e87
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-10-05 10:33:15 +0200

    Removes hack to support reserved keywords for table names

--- plugin-build/src/main/kotlin/ch/rethinc/exposed/generator/ExposedCodeGenerator.kt
+++ plugin-build/src/main/kotlin/ch/rethinc/exposed/generator/ExposedCodeGenerator.kt
@@ -1,6 +1,5 @@
 package ch.rethinc.exposed.generator
 
-import com.squareup.kotlinpoet.FileSpec
 import org.gradle.api.file.Directory
 import org.jetbrains.exposed.gradle.ExposedCodeGenerator
 import org.jetbrains.exposed.gradle.ExposedCodeGeneratorConfiguration
@@ -27,19 +26,15 @@ object ExposedCodeGenerator {
 
 
         val tables = metadataGetter.getTables().filterUtilTables()
-        val config = ExposedCodeGeneratorConfiguration(
-            packageName = packageName,
-            generateSingleFile = false,
-            generatedFileName = null,
-            collate = null,
-            columnMappings = emptyMap()
-        )
+            val config = ExposedCodeGeneratorConfiguration(
+                packageName = packageName,
+                generateSingleFile = false,
+                generatedFileName = null,
+                collate = null,
+                columnMappings = emptyMap()
+            )
         val exposedCodeGenerator = ExposedCodeGenerator(tables, config)
-        val files = exposedCodeGenerator.generateExposedTables().map {
-            // When a table name contains quotes because it is a reserved keyword, the generated file name contains quotes
-            // In these cases toJavaFileObject() will fail because the file name is not a valid file name
-            stripQuotesFromFilename(it)
-        }
+        val files = exposedCodeGenerator.generateExposedTables()
 
         files.forEach {
             it.writeTo(outputDirectory.asFile)
@@ -48,9 +43,6 @@ object ExposedCodeGenerator {
             generatedFile.writeText(ExposedCodeGenerator.postProcessOutput(generatedContent))
         }
     }
-
-    private fun stripQuotesFromFilename(it: FileSpec) =
-        it.toBuilder(it.packageName, it.name.replace("\"", "")).build()
 
     private fun List<schemacrawler.schema.Table>.filterUtilTables() = this.filterNot { it.fullName.startsWith("sys.") }
 
