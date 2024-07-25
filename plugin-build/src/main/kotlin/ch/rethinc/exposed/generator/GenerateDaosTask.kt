90d4d76fa330df4747a490956a416148397ad8b8
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2024-07-25 09:40:12 +0200

    Makes defaultDaoAccessPolicy optional again

--- plugin-build/src/main/kotlin/ch/rethinc/exposed/generator/GenerateDaosTask.kt
+++ plugin-build/src/main/kotlin/ch/rethinc/exposed/generator/GenerateDaosTask.kt
@@ -5,6 +5,7 @@ import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
@@ -27,6 +28,7 @@ abstract class GenerateDaosTask : DefaultTask() {
     abstract val migrationTimeoutMinutes: Property<Long>
 
     @get:Input
+    @get:Optional
     abstract val defaultDaoAccessPolicy: Property<DaoAccessPolicy>
 
     @get:Input
